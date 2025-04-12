47b41638f898f3a42dc51af38888dc9fcd31c7f2
Israr Ahmed <91403838+Israr-11@users.noreply.github.com>
2025-04-12 20:23:51 +0100

    Update newsRoutes.ts

--- routes/newsRoutes.ts
+++ routes/newsRoutes.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import {fetchAndStoreNews} from '../controllers/newsController';
 
 const router = Router();
-
+//Router for the news
 router.post('/news', (req, res) => fetchAndStoreNews(req, res));
 
 export default router;
