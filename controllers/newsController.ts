5c86a3d319122e6c06aa0f3997bf8534b6cc4b8e
Israr Ahmed <91403838+Israr-11@users.noreply.github.com>
2025-04-13 09:21:29 +0100

    Update newsController.ts

--- controllers/newsController.ts
+++ controllers/newsController.ts
@@ -7,7 +7,7 @@ export const fetchAndStoreNews = async (req: Request, res: Response): Promise<vo
     try {
         const { sources } = req.body;
         const articles = await newsService.fetchAndStoreNewsBySource(sources);
-        res.status(200).json(articles);
+         res.status(200).json(articles);
     } catch (error) {
         if (error instanceof Error && error.message === 'No valid news sources provided') {
             res.status(400).json({ error: error.message });
