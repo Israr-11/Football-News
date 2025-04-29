b94ebc5e3c9919f77e5bdff9df44765fbcdc1d0a
Israr Ahmed <91403838+Israr-11@users.noreply.github.com>
2025-04-29 16:03:17 +0100

    Update newsController.ts

--- controllers/newsController.ts
+++ controllers/newsController.ts
@@ -4,6 +4,7 @@ import { NewsService } from '../services/newServices';
 const newsService = new NewsService();
 
 export const fetchAndStoreNews = async (req: Request, res: Response): Promise<void> => {
+
     try {
         const { sources } = req.body;
         const articles = await newsService.fetchAndStoreNewsBySource(sources);
