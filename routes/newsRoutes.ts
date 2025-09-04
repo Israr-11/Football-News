221b02f7c9725626fcc8de46f999f76bc9d4d8f7
Israr A. <91403838+Israr-11@users.noreply.github.com>
2025-09-04 11:03:45 +0100

    Update comment style for newsRoutes

--- routes/newsRoutes.ts
+++ routes/newsRoutes.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 import {fetchAndStoreNews} from '../controllers/newsController';
 
 const router = Router();
-//Router for the news scrapper
+// Router for the news scrapper
 router.post('/news', (req, res) => fetchAndStoreNews(req, res));
 
 export default router;
